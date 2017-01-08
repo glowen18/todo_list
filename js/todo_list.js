27af7795e32d87c8c3ab8a910ea03867d2eecc62
Gaye Lowenstein <glowen@hotmail.com>
2017-01-07 18:13:50 -0800

    completed functionality of adding and removing new todos

--- js/todo_list.js
+++ js/todo_list.js
@@ -1,10 +1,10 @@
 //check off todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500,function(){
     $(this).remove();
   });
@@ -17,6 +17,6 @@ $("input[type='text']").keypress(function(event){
     var todoText = $(this).val();
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li>" + todoText + "</li>")
+    $("ul").append("<li><span>X</span> " + todoText + "</li>")
   }
 });
